9c19ab9591b4c597ab33cc26d02119be7ef4a47c
Eric M Shen <eric.mm.shen@gmai.com>
2021-01-12 16:28:47 -0500

    debug aggregate funcs

--- objection-exercises/src/exercises/06_aggregate.js
+++ objection-exercises/src/exercises/06_aggregate.js
@@ -1,13 +1,20 @@
+const { userParams } = require('../lib')
 const cleanup = require('../lib/cleanup')
 // Import models
+const User = require('../models/User')
+const Pet = require('../models/Pet')
 
 const run = async () => {
   // Get the total number of users
+  const numUsers = await User.query().resultSize()
 
   // Get the average age of users
+  const avgAge = await User.query().avg('age')
 
   // Get the total number of dogs
+  const numDogs = await Pet.query().where('type', 'DOG').resultSize()
 
+  console.log(numUsers, avgAge[0].avg, numDogs)
   // -----
   cleanup()
 }
--- objection-exercises/src/models/User.js
+++ objection-exercises/src/models/User.js
@@ -17,6 +17,30 @@ class User extends BaseModel {
           to: 'pets.ownerId'
         },
       },
+      children: {
+        relation: ManyToManyRelation,
+        modelClass: User,
+        join: {
+            from: 'users.id',
+            through: {
+                from: 'relations.parentId',
+                to: 'relations.childId',
+            },
+            to: 'users.id',
+          },
+        },
+      parents: {
+        relation: ManyToManyRelation,
+        modelClass: User,
+        join: {
+            from: 'users.id',
+            through: {
+                from: 'relations.childId',
+                to: 'relations.parentId',
+            },
+            to: 'users.id',
+          },
+      },
     }
   }
 }
